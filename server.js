93a1978bdcbf1920fd6c933a6039498db12aa21c
sufyaldi <sufyaldys@gmail.com>
2023-09-06 23:16:16 +0700

    c19-crud

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const fs = require('fs').promises;
 const path = require('path');
 const bodyParser = require('body-parser');
-
 const app = express();
 const port = 3001;
 const _dirname = path.resolve();
